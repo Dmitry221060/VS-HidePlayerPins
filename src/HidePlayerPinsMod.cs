4a4f9d148ffa646f5e5e85c90e600a696ad1a6c9
Dmitry221060 <ya.dima42103ya@yandex.ru>
2023-10-03 22:04:57 +0500

    Initial commit

---
+++ src/HidePlayerPinsMod.cs
@@ -0,0 +1,9 @@
+﻿using Vintagestory.API.Common;
+
+namespace HidePlayerPins {
+    public class HidePlayerPinsMod : ModSystem {
+        public override void Start(ICoreAPI api) {
+            api.World.Config.SetBool("mapHideOtherPlayers", true);
+        }
+    }
+}
